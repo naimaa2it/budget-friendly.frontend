aa13a813e53dc216400d5887d72f483e464a9c7e
naima-uddin <toktikproject@gmail.com>
2026-02-16 15:11:49 +0600

    Update page.js

--- app/(main)/dashabord/categories/[id]/page.js
+++ app/(main)/dashabord/categories/[id]/page.js
@@ -1,14 +1,14 @@
 "use client";
 
-import CategoryEditor from '@/components/dashbaord/Category/CategoryEditor';
 import React from 'react';
-
+import CategoryCreate from '@/components/dashbaord/Category/CategoryCreate';
+import CategoryEdit from '@/components/dashbaord/Category/CategoryEdit';
 
 export default function page({ params }) {
   const id = params.id || 'new';
   return (
     <div>
-      <CategoryEditor categoryId={id} />
+      {id === 'new' ? <CategoryCreate /> : <CategoryEdit categoryId={id} />}
     </div>
   );
 }

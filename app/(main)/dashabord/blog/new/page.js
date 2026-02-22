9fdcbf5ff1741731a3d807c505ca0f93537673a2
naima-uddin <toktikproject@gmail.com>
2026-02-22 15:16:16 +0600

    Create page.js

---
+++ app/(main)/dashabord/blog/new/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BlogCreate from '@/components/dashbaord/Blog/BlogCreate';
+
+export default function NewBlogPage() {
+  return (
+    <div className="max-w-4xl mx-auto mt-6">
+      <BlogCreate />
+    </div>
+  );
+}

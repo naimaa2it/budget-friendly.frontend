fdbd49298e2f826d3dacaf52a8e6f4415085d318
naima-uddin <toktikproject@gmail.com>
2026-02-23 10:50:44 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  /* other config options here */
 };
 
 export default nextConfig;

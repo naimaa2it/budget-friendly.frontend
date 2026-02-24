1bb1cc2f66fad4add59d704d8b5c83c2b778da5f
naima-uddin <toktikproject@gmail.com>
2026-02-24 10:43:38 +0600

    Update CartSidebar.jsx

--- components/cart/CartSidebar.jsx
+++ components/cart/CartSidebar.jsx
@@ -27,7 +27,7 @@ export default function CartSidebar() {
 
   return (
     <div
-      className={`fixed top-0 right-0 h-full w-80 bg-white shadow-lg transform transition-transform duration-300 z-60 ${
+      className={`fixed top-0 right-0 h-full w-100 bg-[#ffc2e0]/10 shadow-lg transform transition-transform duration-300 z-50 ${
         isSidebarOpen ? 'translate-x-0' : 'translate-x-full'
       } flex flex-col`}
     >

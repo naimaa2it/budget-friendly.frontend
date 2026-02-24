700f0a84250d6c8acc35ec0225663f4c79b6530f
naima-uddin <toktikproject@gmail.com>
2026-02-24 12:05:15 +0600

    Update RelatedProducts.jsx

--- components/Home/RelatedProducts.jsx
+++ components/Home/RelatedProducts.jsx
@@ -42,7 +42,7 @@ export default function RelatedProducts({ products = [] }) {
             if (scrollRef.current)
               scrollRef.current.scrollBy({ left: scrollRef.current.offsetWidth, behavior: 'smooth' });
           }}
-          className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 rounded-full border border-red-600 bg-white shadow-md hover:bg-gray-100 transition z-10"
+          className="absolute top-1/2 -right-4 transform -translate-y-1/2 p-2 rounded-full border border-red-600 bg-white shadow-md hover:bg-gray-100 transition z-10"
         >
           <FaChevronRight className="w-4 h-4 text-red-600" />
         </button>

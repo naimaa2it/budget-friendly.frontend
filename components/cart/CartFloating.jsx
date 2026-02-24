ef1a8b45e0f813a4bcc7ff07323a3f1a479dee43
naima-uddin <toktikproject@gmail.com>
2026-02-24 12:08:58 +0600

    Update CartFloating.jsx

--- components/cart/CartFloating.jsx
+++ components/cart/CartFloating.jsx
@@ -17,11 +17,11 @@ export default function CartFloating() {
   return (
     <button
       onClick={toggleSidebar}
-      className="fixed right-0 top-1/2 transform -translate-y-1/2 flex items-center gap-2 bg-[#1a1a2e] text-white px-4 py-2 rounded-l-lg shadow-lg z-50 hover:bg-[#16162a] transition"
+      className="fixed right-0 top-1/2 transform -translate-y-1/2 flex items-center gap-2 bg-[#f32424] text-white pl-2 pr-2 py-2.5 rounded-l-lg shadow-lg z-50 hover:bg-[#16162a] transition"
     >
-      <FaShoppingBag className="w-5 h-5" />
-      <div className="flex flex-col items-start text-sm leading-tight">
-        <span>{count} ITEM{count>1?"S":""}</span>
+      <FaShoppingBag className="w-6 h-6" />
+      <div className="flex flex-col items-start text-sm leading-tight ">
+        <span className=''>{count} ITEM{count>1?"S":""}</span>
         <span>৳{total}</span>
       </div>
     </button>

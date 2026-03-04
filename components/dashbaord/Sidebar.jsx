67d2aecfc9e04d33b4e625e29fc18b4cfa2bc36f
naima-uddin <toktikproject@gmail.com>
2026-03-04 09:53:35 +0600

    Update Sidebar.jsx

--- components/dashbaord/Sidebar.jsx
+++ components/dashbaord/Sidebar.jsx
@@ -12,6 +12,7 @@ const nav = [
   { key: 'orders', label: 'Orders', href: '/dashabord/orders', icon: 'M3 3h18v4H3V3z M3 11h18v10H3V11z' },
   { key: 'customers', label: 'Customers', href: '/dashabord/customers', icon: 'M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z M4 20c0-2.21 3.58-4 8-4s8 1.79 8 4' },
   { key: 'reviews', label: 'Reviews', href: '/dashabord/reviews', icon: 'M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z' },
+  { key: 'questions', label: 'Questions', href: '/dashabord/questions', icon: 'M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' },
   { key: 'blog', label: 'Blog / Content', href: '/dashabord/blog', icon: 'M4 7h16M4 11h16M8 15h8' },
   { key: 'media', label: 'Media', href: '/dashabord/media', icon: 'M4 5h16v14H4z M8 9l2 2 3-3 5 5' },
   { key: 'discounts', label: 'Discounts', href: '/dashabord/discounts', icon: 'M12 2l4 4-8 8-4-4 8-8z' },

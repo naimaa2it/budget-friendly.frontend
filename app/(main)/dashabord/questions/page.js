2422836928c276ab9665ed42af6c36b10fcbcffc
naima-uddin <toktikproject@gmail.com>
2026-03-04 09:53:31 +0600

    Create page.js

---
+++ app/(main)/dashabord/questions/page.js
@@ -0,0 +1,8 @@
+import QuestionsList from '@/components/dashbaord/Product/QuestionsList';
+import React from 'react';
+
+export const metadata = { title: 'Questions & Answers — Dashboard' };
+
+export default function QuestionsPage() {
+  return <QuestionsList />;
+}

0971eab9ea58fabd9ec5778c6fb6cde79f6b80db
naima-uddin <toktikproject@gmail.com>
2026-06-16 16:13:32 +0600

    Update page.js

--- app/user/[section]/page.js
+++ app/user/[section]/page.js
@@ -1,6 +1,6 @@
 import UserSectionPage from './PageClient';
 export function generateStaticParams() {
-  return ['profile', 'orders', 'wishlist', 'addresses', 'settings', 'rewards'].map(s => ({ section: s }));
+  return ['profile', 'orders', 'wishlist', 'address', 'reviews', 'rewards', 'loyalty', 'coupons'].map(s => ({ section: s }));
 }
 export default function Page() {
   return <UserSectionPage />;
